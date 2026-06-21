3afc27e1ca90a699ad844c827419a1be4afe3d2b
itzikshol9-arch <itzikshol9@gmail.com>
2026-06-21 22:06:51 +0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MortgageCalculator from "./MortgageCalculator.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <MortgageCalculator />
+  </React.StrictMode>
+);
